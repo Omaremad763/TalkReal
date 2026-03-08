ce8ca9b7ec58929856727487b162b81e28d9f26f
Omar Emad Abusaif <oabuseif125@gmail.com>
2026-03-08 03:13:29 +0200

    Config Fluent Validation and Make Migrations

---
+++ Infra/Extentions/DependenciesCollector.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+
+using Application;
+
+using FluentValidation;
+
+using Infra.Presistence;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+namespace Infra.Extentions;
+public static class DependenciesCollector
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        var issuer = Environment.GetEnvironmentVariable("SaasJWTIssuer");
+        var audience = Environment.GetEnvironmentVariable("SaasJWTAudience");
+        var jwtKey = Environment.GetEnvironmentVariable("SaasJwtKey");
+        var assembly = typeof(IApplicationHandlerMarker).Assembly;
+        var DatabaseConfig = Environment.GetEnvironmentVariable("TalkRealConfig");
+        services.AddDbContext<ApplicationDbContext>
+         (options =>
+         {
+             options.UseNpgsql(DatabaseConfig);
+         });
+        //services.AddScoped<ISaasServices, SaasServices>();
+        //services.AddScoped<IUnitofWork, UnitofWork>();
+        services.AddAutoMapper(cfg => {
+            cfg.AddProfile<AutoMapperProfile>();
+        }, typeof(AutoMapperProfile).Assembly);
+        #region Mediator
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(assembly);
+            cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
+        services.AddValidatorsFromAssembly(assembly);
+        #endregion
+
+        //#region Auth
+        //services.AddAuthentication(options =>
+        //{
+        //    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        //    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        //    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        //})
+        // .AddJwtBearer(options =>
+        //    {
+        //        options.TokenValidationParameters = new TokenValidationParameters
+        //        {
+        //            ValidateIssuer = true,
+        //            ValidateAudience = true,
+        //            ValidateLifetime = true,
+        //            ValidateIssuerSigningKey = true,
+        //            ValidIssuer = issuer,        
+        //            ValidAudience = audience,             
+        //            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
+        //            RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role",
+        //            NameClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"
+        //        };
+        //    });
+        //#endregion
+
+        return services;
+    }
+}
+
---
+++ Infra/Presistence/Flunet Config.cs
@@ -0,0 +1,62 @@
+﻿using Domain.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+namespace Infra.Presistence;
+public class ConversationConfiguration : IEntityTypeConfiguration<Conversation>
+{
+    public void Configure(EntityTypeBuilder<Conversation> builder)
+    {
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Title).HasMaxLength(200);
+
+        builder.HasMany(c => c.Messages)
+               .WithOne()
+               .HasForeignKey(m => m.ConversationId)
+               .OnDelete(DeleteBehavior.Cascade);
+    }
+}
+public class MessageConfiguration : IEntityTypeConfiguration<Message>
+{
+    public void Configure(EntityTypeBuilder<Message> builder)
+    {
+        builder.HasKey(m => m.Id);
+        builder.Property(m => m.Content).IsRequired().HasMaxLength(2000);
+        builder.Property(m => m.SenderId).IsRequired();
+
+        builder.Property(m => m.Status)
+               .HasConversion<string>();
+
+        builder.OwnsOne(m => m.Attachment, a =>
+        {
+            a.Property(p => p.Url).HasColumnName("AttachmentUrl").HasMaxLength(500);
+            a.Property(p => p.Type).HasColumnName("AttachmentType").HasMaxLength(50);
+            a.Property(p => p.Size).HasColumnName("AttachmentSize");
+        });
+    }
+}
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.Property(u => u.LastSeen).IsRequired();
+        builder.Property(u => u.IsOnline).HasDefaultValue(false);
+    }
+}
+
+public class UserConversationConfiguration : IEntityTypeConfiguration<UserConversation>
+{
+    public void Configure(EntityTypeBuilder<UserConversation> builder)
+    {
+        builder.HasKey(uc => new { uc.UserId, uc.ConversationId });
+
+        builder.HasOne(uc => uc.User)
+               .WithMany()
+               .HasForeignKey(uc => uc.UserId);
+
+        builder.HasOne(uc => uc.Conversation)
+               .WithMany()
+               .HasForeignKey(uc => uc.ConversationId);
+    }
+}
