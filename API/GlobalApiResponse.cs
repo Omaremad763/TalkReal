b9aa90f4d0bc674e35d70342002a7bf96e42fa36
Omar Emad Abusaif <oabuseif125@gmail.com>
2026-03-08 03:18:25 +0200

    Add Global Response And Exception Middleware

---
+++ API/GlobalApiResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SaaS_Tenant_Manager
+{
+    public class GlobalApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public T? Data { get; set; }
+        public List<string>? Errors { get; set; }
+        public GlobalApiResponse(T data, bool success = true)
+        {
+            Success = success;
+            Data = data;
+        }
+
+    }
+    public static class ApiResponse
+    {
+        public static GlobalApiResponse<T> Success<T>(T data) => new(data);
+        public static GlobalApiResponse<object?> Success() => new(null);
+        public static GlobalApiResponse<object?> Failure(List<string> errors) => new(null, false) { Errors = errors };
+
+    }
+}
+
+
--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,23 @@
+using API;
+
+using Infra.Extentions;
+using Infra.Presistence;
+
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddServices();       
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
